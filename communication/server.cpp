406a29b46854e25f69092ecac0c49bbf5911d43d
chrolss <chrolss@kth.se>
2015-07-10 15:23:50 +0200

    serverfix for compatability

--- communication/server.cpp
+++ communication/server.cpp
@@ -58,8 +58,8 @@ int main(int argc, char *argv[])
     	 bzero(buffer,256);
      	 n = read(newsockfd,buffer,255);
      	 if (n < 0) error("ERROR reading from socket");
-     	 printf("Here is the message: %s",buffer);
-
+     	 printf("Here is the message: %s \n",buffer);
+     	 i = 3;
      	 //break communication if client sends "quit"
      	 std::string str(buffer);
      	 std::string subst = str.substr(0,4);
